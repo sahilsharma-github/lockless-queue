0565e7d5535b8bedf3d918e4490ba49a37d45239
Sahil Sharma <sahil.sharma@iongroup.com>
2019-08-12 10:40:30 +0530

    Development Phase

--- lockless_queue/test.cpp
+++ lockless_queue/test.cpp
@@ -2,6 +2,7 @@
 #include<thread>
 #include <fstream>
 #include<sstream>
+#include<math.h>
 
 using namespace std;
 
@@ -20,13 +21,13 @@ void produce()
 	//static int i = 0;
 	for (i; i <= 1000000; ++i)
 	{
-		++l;
+		//++l;
 		lq.enqueue(i);
 	}
 }
 
 int j = 1;
-atomic<int>k = 0;
+//atomic<int>k = 0;
 void consume()
 {
 	auto myid = this_thread::get_id();
@@ -46,10 +47,12 @@ void consume()
 	}*/
 	while (true)
 	{
-		if (lq.dequeue(data))
+		/*if (lq.dequeue(data))
 			consumer1 << data << '\n';
 		else
-			break;
+			break;*/
+		/*if (!lq.dequeue(data))
+			return;*/
 	}
 }
 
@@ -58,25 +61,47 @@ int main()
 {
 	lq.initialize(1000);
 	thread tp1(&produce);
+	thread tp2(&produce);
 	tp1.join(); cout << "tp1 joined\n";
-	/*thread tp2(&produce);
+	tp2.join(); cout << "tp2 joined\n";
+	exit(0);
+	
 	thread tp3(&produce);
 	thread tp4(&produce);
-	thread tp5(&produce);*/
+	thread tp5(&produce);
+	
+	
+	tp3.join(); cout << "tp1 joined\n";
+	tp4.join(); cout << "tp1 joined\n";
+	tp5.join(); cout << "tp1 joined\n";
+	
+
+
+
 	thread tc1(&consume);
 	thread tc2(&consume);
 	thread tc3(&consume);
 	thread tc4(&consume);
 	thread tc5(&consume);
+
 	 /*tp2.join(); cout << "tp2 joined\n"; tp3.join(); 
 	cout << "tp3 joined\n"; tp4.join(); cout << "tp4 joined\n"; tp5.join();
 	cout << "tp5 joined\n";*/
+	
+
 	tc1.join(); cout << "tc1 joined\n"; tc2.join(); cout << "tc2 joined\n";tc3.join(); tc4.join(); tc5.join();
+
 	//tc1.join();
+
 	std::cout << "Tc5 joined\n";
+
+
 	//printQueue();
 	//std::cout << "T2 joined\n";
-	std::cout << "l is : " << l.load() << std::endl;
-	std::cout << "k is : " << k.load() << std::endl;
+
+
+	//std::cout << "l is : " << l.load() << std::endl;
+	//std::cout << "k is : " << k.load() << std::endl;
+	
 	return 0;
 }
